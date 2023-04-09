67d72d4186ef8aa8069fd4c1ff2513d2b8ff4619
Pcrab <pcrab@pcrab.xyz>
2023-04-09 16:16:32 +0800

    perf: remove deasync for better performance

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-try {
-    const config = (hexo.config.shiki = Object.assign(
-        {
-            theme: "github-light",
-        },
-        hexo.config.shiki || {}
-    ));
-
-    const highlighter = require("./getHighlighter.js")(config)();
-
-    hexo.extend.highlight.register("shiki", require("./lib.js")(highlighter));
-} catch (e) {
-    console.error(e);
-}
+const shiki = require("shiki");
+const config = (hexo.config.shiki = Object.assign(
+    {
+        theme: "github-light",
+    },
+    hexo.config.shiki || {}
+));
+return shiki
+        .getHighlighter({
+            theme: config.theme,
+        })
+        .then((hl) => {
+            hexo.extend.highlight.register("shiki", (code, options) => {
+                try {
+                    return hl.codeToHtml(code, { lang: options.lang });
+                } catch (e) {
+                    console.error(e);
+                    return `<pre><code>${code}</code></pre>`;
+                }
+            });
+        });
