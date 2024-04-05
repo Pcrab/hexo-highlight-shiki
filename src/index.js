faf723d331797d56e2eb068e98a29cd783da5186
pcrab <pcrab@pcrab.xyz>
2024-04-05 22:53:58 +0800

    feat: use top-level await

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const shiki = require("shiki");
+
 const fsPromises = require("fs/promises");
 const config = (hexo.config.shiki = Object.assign(
     {
@@ -8,41 +9,39 @@ const config = (hexo.config.shiki = Object.assign(
     },
     hexo.config.shiki || {}
 ));
-return shiki
-    .getHighlighter({
-        theme: config.theme,
-    })
-    .then(async (hl) => {
-        for (const lang of config.languages) {
-            const grammarDef = JSON.parse(await fsPromises.readFile(lang.grammar));
-            const langDef = {
-                id: lang.id,
-                scopeName: lang.scope_name,
-                grammar: grammarDef,
-                aliases: lang.aliases || [],
-            };
-            await hl.loadLanguage(langDef);
-        }
-        if (config.renderer === "djot") {
-            hexo.extend.filter.register("djot:renderer", (renderOverrides) => {
-                renderOverrides.code_block = (node) => {
-                    const { lang, text } = node;
-                    try {
-                        return hl.codeToHtml(text, { lang });
-                    } catch (e) {
-                        console.error(e);
-                        return `<pre><code>${text}</code></pre>`;
-                    }
-                };
-            });
-        } else {
-            hexo.extend.highlight.register("shiki", (code, options) => {
-                try {
-                    return hl.codeToHtml(code, { lang: options.lang });
-                } catch (e) {
-                    console.error(e);
-                    return `<pre><code>${code}</code></pre>`;
-                }
-            });
+
+const hl = await shiki.getHighlighter({
+    theme: config.theme,
+});
+await Promise.allSettled(config.languages.map(async (lang) => {
+    const grammarDef = JSON.parse(await fsPromises.readFile(lang.grammar));
+    const langDef = {
+        id: lang.id,
+        scopeName: lang.scope_name,
+        grammar: grammarDef,
+        aliases: lang.aliases || [],
+    };
+    await hl.loadLanguage(langDef);
+}))
+if (config.renderer === "djot") {
+    hexo.extend.filter.register("djot:renderer", (renderOverrides) => {
+        renderOverrides.code_block = (node) => {
+            const { lang, text } = node;
+            try {
+                return hl.codeToHtml(text, { lang });
+            } catch (e) {
+                console.error(e);
+                return `<pre><code>${text}</code></pre>`;
+            }
+        };
+    });
+} else {
+    hexo.extend.highlight.register("shiki", (code, options) => {
+        try {
+            return hl.codeToHtml(code, { lang: options.lang });
+        } catch (e) {
+            console.error(e);
+            return `<pre><code>${code}</code></pre>`;
         }
     });
+}
